8b1fa07374dbdac22c9add47b8c959d6e66d9b82
maria <winterzaq1@gmail.com>
2024-03-22 20:23:30 +0900

    implement iter for ex01

--- cpp_07/ex01/includes/iter.hpp
+++ cpp_07/ex01/includes/iter.hpp
@@ -1,12 +1,24 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
-#include <stddef.h>
+#include <stddef.h> //size_t
 
-//so what i don't get enough about type T. should i do it with a const T &?
+//could alter contents
 template <typename T>
-void iter(T *array, size_t length, void (*f)(T)) {
-	for (size_t i = 0; i < length; i++)  {
+void iter(T *array, size_t length, void (*f)(T&)) {
+	if (!array || !f)
+		return;
+	for (size_t i = 0; i < length; i++) {
+		f(array[i]);
+	}
+}
+
+//doesn't alter contents
+template <typename T>
+void iter(const T *array, size_t length, void (*f)(const T&)) {
+	if (!array || !f)
+		return;
+	for (size_t i = 0; i < length; i++) {
 		f(array[i]);
 	}
 }
